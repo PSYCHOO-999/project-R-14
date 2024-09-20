fa5dc82ed3ecfb0db60b65a2d5b182fc90507dd2
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2024-09-20 12:25:28 +0530

    first React Code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div>
+      <div>
+        <h1>Hello Everyone</h1>
+      </div>
+      <div>
+        <p>React 18 is awesome</p>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
